3b21c9f55e144d4e7b261d3216e431f9b3955a55
Declan Boller <declanjboller@outlook.com>
2018-12-20 12:04:47 +1100

    Changed Styles, attempt implementing smart computer

--- js/logic.js
+++ js/logic.js
@@ -1,8 +1,9 @@
+// Assign Noughts and Crosses Icons
 const $cross = '<i class="fas fa-times"></i>';
 const $noughts = '<i class="far fa-circle"></i>';
 
 
-
+// Baseline Game Info
 const game = {
   currentplayer: 'X',
   player1: '',
@@ -13,14 +14,14 @@ const game = {
 };
 
 
-
+// Players Score
 const score = {
   X: [],
   O: [],
 };
 
 
-
+// All possible combos of Wins
 const winningCombos = [
   [1, 2, 3],
   [4, 5, 6],
@@ -32,18 +33,26 @@ const winningCombos = [
   [3, 5, 7],
   ];
 
+// Smart Computer -- 
+
+// Loop through the Winning Combos
+// Loop through the other players score
+// If OP's tile is filled *Ignore* and place in the next available spot
 
 
+
+
+// Fireworks Canvas Parameters
 const fireworks = function () {
   $('.winner').append(canvas);
   canvas.width = SCREEN_WIDTH;
   canvas.height = SCREEN_HEIGHT;
-  setInterval(launch, 200);
+  setInterval(launch, 100);
   setInterval(loop, 700 / 50);
 };
 
 
-
+// Init Fireworks && Hide Fireworks
 const fireworksDisplay = function () {
   $('.winner').fadeIn(1500);
   setTimeout(function () {
@@ -53,61 +62,66 @@ const fireworksDisplay = function () {
 };
 
 
-
+// Assign Player Based off Input from Buttons -- Default Player is X (game.currentplayer = 'X' --
 function assignPlayer() {
   $('#X').on('click', function () {
     game.player1 = 'X';
     game.player2 = 'O';
     game.currentplayer = 'X';
-    $('#X').addClass('active');
-    $('#O').removeClass('active');
+    $('#X').addClass('activeX');
+    $('#O').removeClass('activeO');
   });
   $('#O').on('click', function () {
     game.player1 = 'O';
     game.player2 = 'X';
     game.currentplayer = 'O';
-    $('#O').addClass('active');
-    $('#X').removeClass('active');
+    $('#O').addClass('activeO');
+    $('#X').removeClass('activeX');
   });
 };
 
 
-
+// Clears all Icons, Resets Score, Resets Move Count, Re-Enables Clicks, Sets Current Player Back to X
 const reset = function () {
   $('.grid-container .box').html('');
   score.X.length = 0;
   score.O.length = 0;
   $('.grid-container .box').attr('disabled', false);
   game.currentplayer = 'X';
-  $('#X').addClass('active');
-  $('#O').removeClass('active');
+  $('#X').addClass('activeX');
+  $('#O').removeClass('activeO');
   game.moves = 0;
   $('h1').html('Noughts &amp; Crosses').hide().fadeIn(2500);
 };
 
 
-
+// Loops Through Winning Combos && Finds if the Index Matches the Players Moves 
 const win = function () {
   for (let i = 0; i < winningCombos.length; i++) {
     let currentCombos = winningCombos[i];
     var Winner_X = score.X.includes(currentCombos[0]) && score.X.includes(currentCombos[1]) && score.X.includes(currentCombos[2]);
     var Winner_O = score.O.includes(currentCombos[0]) && score.O.includes(currentCombos[1]) && score.O.includes(currentCombos[2]);
 
+// Disables Specified Button, Adds +1 to gamesWon, Sets Fireworks Display off, Changes H1
     if (Winner_X) {
       $('.grid-container .box').attr('disabled', true);
       game.gamesWonX++;
       fireworksDisplay();
       $('#PlayerX').html(game.gamesWonX);
       $('h1').html('Crosses Wins').hide().fadeIn(2000);
+      return
     } else if (Winner_O) {
       $('.grid-container .box').attr('disabled', true);
       game.gamesWonO++;
       fireworksDisplay();
       $('#PlayerO').html(game.gamesWonO);
       $('h1').html('Noughts Wins').hide().fadeIn(2000);
+      return
     }
   }
-  if (!Winner_O && !Winner_X && game.moves === 9) {
+  
+// If Nothing Above Returns True, Its a Draw, Reset Count, Change H1 
+  if (game.moves >= 9) {
     $('h1').html('Draw').hide().fadeIn(2000);
     setTimeout(function () {
       reset();
@@ -116,22 +130,25 @@ const win = function () {
 };
 
 
-
+// Checks Current Player, adds Icon to button, Sends ID of button to score array, Switch players
 const check = function (element) {
   if (game.currentplayer === 'X') {
     $(element).html($($cross).hide().fadeIn(300));
     score.X.push(Number($(element).attr('id')));
-    $('#X').removeClass('active');
-    $('#O').addClass('active');
+    $('#X').removeClass('activeX');
+    $('#O').addClass('activeO');
     game.currentplayer = 'O';
   } else {
     $(element).html($($noughts).hide().fadeIn(300));
     score.O.push(Number($(element).attr('id')));
     game.currentplayer = 'X';
-    $('#O').removeClass('active');
-    $('#X').addClass('active');
+    $('#O').removeClass('activeO');
+    $('#X').addClass('activeX');
   };
+// Disables clicked Button
   $(element).attr('disabled', true);
+// Sends +1 Moves to game.moves array
   game.moves++
+// Check for Win
   win();
 };
